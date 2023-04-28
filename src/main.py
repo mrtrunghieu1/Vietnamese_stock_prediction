805e32709a2c71f9a8704e86278b3485361a5186
mrtrunghieu1 <trunghieu02111998@gmail.com>
2023-04-28 16:53:16 +0700

    Change main file

--- src/main.py
+++ src/main.py
@@ -27,18 +27,16 @@ for stock_company in stock_companies:
         df.to_csv(os.path.join(processed_data_path, '{}.csv'.format(stock_company)))
     # Data Processing the stock market
     processed_companies = os.listdir(processed_data_path)
-    try:
-        check_stock_existence(stock_company, processed_companies)
-        print(datetime.datetime.now(), 'Processing the stock of {} company'.format(stock_company))
-        file_csv = os.path.join(processed_data_path, '{}.csv'.format(stock_company))
-        df = pd.read_csv(file_csv)
-        df['date'] = df['date'].astype("datetime64[ns]")
-        scaler, scaled_data, dataframe = data_preprocessing(df)
-        train_dates, X_train, y_train, val_dates, X_val, y_val, test_dates, X_test, y_test = split_data(dataframe,
-                                                                                                        scaled_data)
-        # Train LSTM model
-        model = build_model(X_train, y_train, X_val, y_val)
-        test_predictions = model.predict(X_test)
-        print(test_predictions.shape)
-    except ValueError as err:
-        print(f"Error: {err}")
+    check_stock_existence(stock_company, processed_companies)
+    print(datetime.datetime.now(), 'Processing the stock of {} company'.format(stock_company))
+    file_csv = os.path.join(processed_data_path, '{}.csv'.format(stock_company))
+    df = pd.read_csv(file_csv)
+    df['date'] = df['date'].astype("datetime64[ns]")
+    scaler, scaled_data, dataframe = data_preprocessing(df)
+    train_dates, X_train, y_train, val_dates, X_val, y_val, test_dates, X_test, y_test = split_data(dataframe,
+                                                                                                    scaled_data)
+    # Train LSTM model
+    model = build_model(X_train, y_train, X_val, y_val)
+    test_predictions = model.predict(X_test)
+    print(test_predictions.shape)
+
